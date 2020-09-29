1a3df4a17fc88d27b4546809cf7196033f660f80
Mattia <terefeit@gmail.com>
2020-09-30 01:09:17 +0200

    AOOO

--- pages/cultura-medica.js
+++ pages/cultura-medica.js
@@ -45,19 +45,20 @@ export async function getStaticProps(){
    return allElements;
    }
 
-let categories = await getData("categories")
+//let categories = await getData("categories")
 let posts = await  getData("posts")
-let pages = await  getData("pages")
-let users = await getData("users")
-let tags = await getData("tags")
+//let pages = await  getData("pages")
+//let users = await getData("users")
+//let tags = await getData("tags")
 
   return {props: {
-    categories: categories,
+    //categories: categories,
    posts: posts,
-   pages: pages,
-   users: users, 
+   //pages: pages,
+   //users: users, 
   }}
 }
+
 
 
 
