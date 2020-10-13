5a4d0ef806013fcfcc6c66b4355b04ce9d456058
MattiaTerefe <terefeit@gmail.com>
2020-10-13 21:54:07 +0200

    filtered pages

--- public/components/pages.js
+++ public/components/pages.js
@@ -2,8 +2,8 @@
 import Link from "next/link";
 
 export const Pages = (props)=>{
-const pageButtons = props.pages.map((el)=><div className="row mb-2"><Link href={"/pagine/" + el.slug}><a>{el.title.rendered}</a></Link></div>)
-
+let pageButtons = props.pages.map((el)=><div className="row mb-2"><Link href={"/pagine/" + el.slug}><a>{el.title.rendered}</a></Link></div>)
+pageButtons = pageButtons.filter((el)=>el.slug==="pagine-amiche" || el.slug==="chi-siamo" || el.slug==="contatti")
 return (<div>
     <h2>PAGINE</h2>
 <div className="container">
