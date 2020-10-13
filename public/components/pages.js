17b1ff2c5cbd0ffdbe54930b83e7db744264c803
MattiaTerefe <terefeit@gmail.com>
2020-10-13 21:57:57 +0200

    fixed stuff

--- public/components/pages.js
+++ public/components/pages.js
@@ -2,8 +2,8 @@
 import Link from "next/link";
 
 export const Pages = (props)=>{
-let pageButtons = props.pages.map((el)=><div className="row mb-2"><Link href={"/pagine/" + el.slug}><a>{el.title.rendered}</a></Link></div>)
-pageButtons = pageButtons.filter((el)=>el.slug==="pagine-amiche" || el.slug==="chi-siamo" || el.slug==="contatti")
+let filtered = props.pages.filter((el)=>el.slug==="pagine-amiche" || el.slug==="chi-siamo" || el.slug==="contatti")
+let pageButtons = filtered.map((el)=><div className="row mb-2"><Link href={"/pagine/" + el.slug}><a>{el.title.rendered}</a></Link></div>)
 return (<div>
     <h2>PAGINE</h2>
 <div className="container">
