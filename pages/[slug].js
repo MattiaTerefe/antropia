fb1eecc53d785d1a92d59929c433c329b3ebd83b
Mattia Terefe <51480011+MattiaTerefe@users.noreply.github.com>
2020-10-23 10:08:05 +0200

    Header cambia il sottotitolo in base al post

--- pages/[slug].js
+++ pages/[slug].js
@@ -37,7 +37,7 @@ fallback: false,
 export default function Post({post, categories, pages}){
     
     return( <>
-    <Header current={"HOME"}/>
+    <Header current={post.title.rendered}/>
   <div className="container">
     <div className="row">
     <div className="col-2">
